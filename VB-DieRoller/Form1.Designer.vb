18d37a688477d1bec0097f1c16571614e32a1a04
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2023-09-03 21:00:45 -0500

    Main code commit.
    This is all the source code for the main project.

---
+++ VB-DieRoller/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        OutBox = New ListBox()
+        CmbDieType = New ComboBox()
+        NumBoxDice = New NumericUpDown()
+        BtnRoll = New Button()
+        GrpStats = New GroupBox()
+        Rdo4d6 = New RadioButton()
+        Rdo3d6 = New RadioButton()
+        BtnAbilityRoll = New Button()
+        BtnClear = New Button()
+        CType(NumBoxDice, ComponentModel.ISupportInitialize).BeginInit()
+        GrpStats.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(15, 11)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(99, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Num Dice: "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(15, 76)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Die Type: "
+        ' 
+        ' OutBox
+        ' 
+        OutBox.FormattingEnabled = True
+        OutBox.ItemHeight = 25
+        OutBox.Location = New Point(12, 165)
+        OutBox.Name = "OutBox"
+        OutBox.Size = New Size(1129, 154)
+        OutBox.TabIndex = 2
+        ' 
+        ' CmbDieType
+        ' 
+        CmbDieType.FormattingEnabled = True
+        CmbDieType.Items.AddRange(New Object() {"D20", "D12", "D10", "D8", "D6", "D4", "D100"})
+        CmbDieType.Location = New Point(111, 73)
+        CmbDieType.Name = "CmbDieType"
+        CmbDieType.Size = New Size(401, 33)
+        CmbDieType.TabIndex = 4
+        ' 
+        ' NumBoxDice
+        ' 
+        NumBoxDice.Location = New Point(121, 9)
+        NumBoxDice.Name = "NumBoxDice"
+        NumBoxDice.Size = New Size(391, 31)
+        NumBoxDice.TabIndex = 5
+        ' 
+        ' BtnRoll
+        ' 
+        BtnRoll.Location = New Point(1019, 7)
+        BtnRoll.Name = "BtnRoll"
+        BtnRoll.Size = New Size(122, 33)
+        BtnRoll.TabIndex = 6
+        BtnRoll.Text = "Roll"
+        BtnRoll.UseVisualStyleBackColor = True
+        ' 
+        ' GrpStats
+        ' 
+        GrpStats.Controls.Add(Rdo4d6)
+        GrpStats.Controls.Add(Rdo3d6)
+        GrpStats.Location = New Point(590, 9)
+        GrpStats.Name = "GrpStats"
+        GrpStats.Size = New Size(254, 125)
+        GrpStats.TabIndex = 7
+        GrpStats.TabStop = False
+        GrpStats.Text = "Ability Roll Controls"
+        ' 
+        ' Rdo4d6
+        ' 
+        Rdo4d6.AutoSize = True
+        Rdo4d6.Location = New Point(6, 90)
+        Rdo4d6.Name = "Rdo4d6"
+        Rdo4d6.Size = New Size(64, 29)
+        Rdo4d6.TabIndex = 1
+        Rdo4d6.TabStop = True
+        Rdo4d6.Text = "4d6"
+        Rdo4d6.UseVisualStyleBackColor = True
+        ' 
+        ' Rdo3d6
+        ' 
+        Rdo3d6.AutoSize = True
+        Rdo3d6.Location = New Point(6, 30)
+        Rdo3d6.Name = "Rdo3d6"
+        Rdo3d6.Size = New Size(64, 29)
+        Rdo3d6.TabIndex = 0
+        Rdo3d6.TabStop = True
+        Rdo3d6.Text = "3d6"
+        Rdo3d6.UseVisualStyleBackColor = True
+        ' 
+        ' BtnAbilityRoll
+        ' 
+        BtnAbilityRoll.Location = New Point(1019, 97)
+        BtnAbilityRoll.Name = "BtnAbilityRoll"
+        BtnAbilityRoll.Size = New Size(122, 33)
+        BtnAbilityRoll.TabIndex = 8
+        BtnAbilityRoll.Text = "Ability Roll"
+        BtnAbilityRoll.UseVisualStyleBackColor = True
+        ' 
+        ' BtnClear
+        ' 
+        BtnClear.Location = New Point(1019, 52)
+        BtnClear.Name = "BtnClear"
+        BtnClear.Size = New Size(122, 33)
+        BtnClear.TabIndex = 9
+        BtnClear.Text = "Clear Output"
+        BtnClear.UseVisualStyleBackColor = True
+        ' 
+        ' FrmMain
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1153, 331)
+        Controls.Add(BtnClear)
+        Controls.Add(BtnAbilityRoll)
+        Controls.Add(GrpStats)
+        Controls.Add(BtnRoll)
+        Controls.Add(NumBoxDice)
+        Controls.Add(CmbDieType)
+        Controls.Add(OutBox)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Margin = New Padding(4)
+        Name = "FrmMain"
+        Text = "VB Die Roller"
+        CType(NumBoxDice, ComponentModel.ISupportInitialize).EndInit()
+        GrpStats.ResumeLayout(False)
+        GrpStats.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents OutBox As ListBox
+    Friend WithEvents CmbDieType As ComboBox
+    Friend WithEvents NumBoxDice As NumericUpDown
+    Friend WithEvents BtnRoll As Button
+    Friend WithEvents GrpStats As GroupBox
+    Friend WithEvents Rdo4d6 As RadioButton
+    Friend WithEvents Rdo3d6 As RadioButton
+    Friend WithEvents BtnAbilityRoll As Button
+    Friend WithEvents BtnClear As Button
+End Class
---
+++ VB-DieRoller/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class FrmMain
+    Private Sub BtnRoll_Click(sender As Object, e As EventArgs) Handles BtnRoll.Click
+        Dim NumDice = Integer.Parse(NumBoxDice.Value)
+        Dim DieRoll = Roll(NumDice, CmbDieType.SelectedItem)
+
+        OutBox.Items.Add($"You just rolled {DieRoll}!")
+    End Sub
+
+    Private Sub BtnAbilityRoll_Click(sender As Object, e As EventArgs) Handles BtnAbilityRoll.Click
+        Dim Result As Integer = RollAbility(Rdo3d6.Checked, Rdo4d6.Checked)
+        Dim Method As String
+        If Rdo3d6.Checked Then
+            Method = "3d6"
+        ElseIf Rdo4d6.Checked Then
+            Method = "4d6"
+        Else
+            Method = "Undefined"
+        End If
+        OutBox.Items.Add($"Rolling ability using the {Method} method. Result: {Result}!!")
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        NumBoxDice.Value = 0
+        CmbDieType.SelectedValue = ""
+        Rdo3d6.Checked = False
+        Rdo4d6.Checked = False
+        OutBox.Items.Clear()
+    End Sub
+
+    Private Function Roll(NumDice As Integer, DieType As String) As Integer
+        Dim NumSides As Integer = GetSides(DieType)
+        Dim TotalRoll As Integer
+        Dim Die As Integer
+        For i As Integer = 1 To NumDice
+            Die = (Rnd() * NumSides) + 1
+            If Die > NumSides Then
+                Die = NumSides
+            End If
+            TotalRoll += Die
+        Next
+        Return TotalRoll
+    End Function
+
+    Private Function GetSides(DieType As String) As Integer
+        Dim NumSides As Integer
+        Select Case DieType
+            Case "D20"
+                NumSides = 20
+            Case "D12"
+                NumSides = 12
+            Case "D10"
+                NumSides = 10
+            Case "D8"
+                NumSides = 8
+            Case "D6"
+                NumSides = 6
+            Case "D4"
+                NumSides = 4
+            Case "D100"
+                NumSides = 100
+        End Select
+        Return NumSides
+    End Function
+
+    Private Function RollAbility(M1 As Boolean, M2 As Boolean) As Integer
+        Dim NumDice, Die, TotalRoll As Integer
+        Dim DieList As List(Of Integer) = New List(Of Integer)
+        If M1 Then
+            NumDice = 3
+
+            For i As Integer = 1 To NumDice
+                Die = (Rnd() * 6) + 1
+                If Die > 6 Then
+                    Die = 6
+                End If
+                TotalRoll += Die
+            Next
+        ElseIf M2 Then
+            NumDice = 4
+
+            For j As Integer = 1 To NumDice
+                Die = (Rnd() * 6) + 1
+                DieList.Add(Die)
+            Next
+            DieList.Sort()
+            DieList.Remove(0)
+            TotalRoll = DieList.Sum()
+        Else
+            MessageBox.Show("Please select one of the Radio Buttons")
+        End If
+
+        Return TotalRoll
+    End Function
+End Class
